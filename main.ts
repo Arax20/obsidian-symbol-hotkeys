e1e9675fdad938528023a04b89dfa83c58ff0fcc
Putter <chantapat@protonmail.com>
2021-04-21 15:29:41 +0700

    first commit

--- main.ts
+++ main.ts
@@ -1,112 +1,67 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+import {Editor, MarkdownView, Plugin} from 'obsidian';
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
 	async onload() {
 		console.log('loading plugin');
 
-		await this.loadSettings();
-
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
+		this.addCommand({
+			id:"insert-math",
+			name:"Insert math wrapper",
+			callback: () => this.insertMath(),
+			hotkeys: [
+			{
+				modifiers: ["Mod"],
+				key: "m"
+			}],
 		});
-
-		this.addStatusBarItem().setText('Status Bar Text');
 
 		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-					return true;
-				}
-				return false;
-			}
-		});
-
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
-		});
-
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+			id:"insert-mhchem",
+			name:"Insert chemistry wrapper",
+			callback: () => this.insertChem(),
+			hotkeys: [
+			{
+				modifiers: ["Mod", "Alt"],
+				key: "m"
+			}]
+		})
 	}
+
+	insertMath(): void {
+		const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+		if (!view) return;
+		const editor = view.editor;
+
+		let selectedText = editor.somethingSelected()
+		? editor.getSelection()
+		: false;
+		if (selectedText) {
+			editor.replaceSelection(`\$${selectedText}\$`);
+		} else {
+			editor.replaceSelection("\$\$");
+			editor.exec("goLeft");
+		}
+	}
+
+	insertChem(): void {
+		const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+		if (!view) return;
+		const editor = view.editor;
+
+		let selectedText = editor.somethingSelected()
+		? editor.getSelection()
+		: false;
+		if (selectedText) {
+			editor.replaceSelection(`\$\\ce{${selectedText}}\$`);
+		} else {
+			editor.replaceSelection("\$\\ce{}\$");
+			editor.exec("goLeft");
+			editor.exec("goLeft");
+		}
+	}
+
 
 	onunload() {
 		console.log('unloading plugin');
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		let {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
